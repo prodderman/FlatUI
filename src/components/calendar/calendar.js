40101c0ab25c7ff1447c67eee6f04ca78b790e0e
Степан <rainbowprodder@gmail.com>
2017-09-07 01:04:49 +0700

    Допилин календарь

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -6,20 +6,38 @@ import './calendar.styl';
 class Calendar {
   constructor(calendar) {
     this.calendar = $(calendar);
-    this.init();
+    this.Init();
+    this.EventHandlers();
   }
-  init() {
+
+  EventHandlers() {  }
+
+  Init() {
     const nowNode = this.calendar.children(".calendar__now");
+    const _goToTodaySave = $.datepicker._gotoToday;
+    const _selectDateSave = $.datepicker._selectDate;
     nowNode.text(new Date().getDate());
 
+    $.datepicker._selectDate = function(id, dateStr) {
+      _selectDateSave.call(this, id, dateStr);
+      $( id ).datepicker("setDate", dateStr);
+    };
+
+    $.datepicker._gotoToday = function(id) {
+      _goToTodaySave.call(this,id);
+      this._selectDate(id);
+    };
+    
     this.calendar.datepicker({
       inline: true,
       selectOtherMonths: true,
-      
       dayNamesMin: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
       showButtonPanel: true,
       showOtherMonths: true,
       firstDay: 1,
+      onSelect: function(date, inst) {
+        nowNode.text(new Date(date).getDate());
+      }
     });
   }
 }
