9f5793cce210e05554c95cf7c84f3aa77229acf0
Степан <rainbowprodder@gmail.com>
2017-09-09 01:02:30 +0700

    ajax submit формы // onresize для видоса // правка некоторых стилей

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -2,7 +2,32 @@ import './form.styl';
 import 'parsleyjs';
 
 class Feedback {
-  constructor() {
-    
+  constructor(form) {
+    this.form = $(form);
+    this.EventHandlers();
+  }
+
+  EventHandlers() { 
+    this.form.submit((e) => {
+      e.preventDefault();
+      $.ajax({
+        type: this.form.attr("method"),
+        url: this.form.attr("action"),
+        data: this.form.serializeArray(),
+        success: () => {
+          this.form.trigger('reset');
+          this.form.find("input, select, textarea").trigger("focusout");
+          this.form.parsley().reset();
+        },
+        error: (xhr, textStatus, errorThrown) => {
+          alert(`error: ${errorThrown ? errorThrown : xhr.status} ${textStatus}`);
+        },
+        
+      });
+    });
   }
 }
+
+$(()=> {
+  const forms = $('.js-feedback').map((index, node) => new Feedback(node));
+});
--- src/components/video/video.js
+++ src/components/video/video.js
@@ -4,6 +4,16 @@ class Video {
   constructor(video) {  
     this.video = $(video);
     this.Init();
+    this.EventHandlers();
+  }
+
+  EventHandlers() {  
+    $( window ).resize(() => {
+      const size = this.SetSize();
+      const frame = this.video.children("iframe");
+      frame.width(size.width);
+      frame.height(size.height);
+    });
   }
   
   get options() {
@@ -12,27 +22,35 @@ class Video {
 
   static get snippets() {
     return {
-      'youtube': "https://www.youtube.com/embed/",
-      'youtu': "https://www.youtube.com/embed/",
-      'vimeo': "https://player.vimeo.com/video/",
-      'rutube': "https://rutube.ru/play/embed/"
+      'youtube': "//www.youtube.com/embed/",
+      'youtu': "//www.youtube.com/embed/",
+      'vimeo': "//player.vimeo.com/video/",
+      'rutube': "//rutube.ru/play/embed/",
+      "vk": ""
     };
   }
 
   Init() {
-    const src = new URL(this.video.data("src"));
-    const width = this.video.data('format').split(":")[0];
-    const height = this.video.data('format').split(":")[1];
-    const frameWidth = this.video.width();
-    const frameHeight = height*frameWidth/width;
+    const url = this.video.data("src")
+    const src = url.match(/https:|http:/) ? new URL(url) : new URL(`https:${url}`);
+    const size = this.SetSize();
     const frame = $('<iframe/>', {
-      width: frameWidth,
-      height: frameHeight,
+      width: size.width,
+      height: size.height,
       src: this.CreateURL(src),
       allowfullscreen: "",
       frameborder: 0
     });
+    this.SetSize(frame);
     this.video.append(frame);
+  }
+
+  SetSize() {
+    const width = this.video.data('format').split(":")[0];
+    const height = this.video.data('format').split(":")[1];
+    const frameWidth = this.video.width();
+    const frameHeight = height*frameWidth/width;
+    return {width: frameWidth, height: frameHeight};
   }
   
   CreateURL(src) {
@@ -40,6 +58,8 @@ class Video {
       if (key === this.TestSrc(src.hostname)) {
         if (key === 'youtube')
           return `${Video.snippets[key]}${this.TestId(src.search)[1]}`;
+        else if (key === 'vk')
+          return src;
         else
           return `${Video.snippets[key]}${this.TestId(src.pathname).slice(-1)[0]}`;
       }
