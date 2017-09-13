f2c13f532fff747b9e23248210ca0ab2044aab28
Степан <rainbowprodder@gmail.com>
2017-09-14 03:10:01 +0700

    Яндекс-карта

---
+++ src/components/map/map.js
@@ -0,0 +1,34 @@
+import './map.styl'; 
+
+class Map {
+  constructor(node) {
+    this.map = $(node);
+    this.init();
+  }
+
+  init() {
+    const coord = this.map.data('coord');
+    const toMarker = this.map.find('.map__to-marker');
+    if (!(coord instanceof Array)) return;
+    ymaps.ready(() => {
+      const map = new ymaps.Map($('.js-map__location', this.map)[0], {
+        center: coord,
+        zoom: 17,
+        controls: []
+      });
+
+      const placemark = new ymaps.Placemark(coord, {}, {
+        iconLayout: 'default#image',
+        iconImageHref: require('./img/marker.png'),
+        iconImageSize: [55, 55],
+        iconImageOffset: [-19, -56]
+      });
+      map.geoObjects.add(placemark);
+      toMarker.click((e) => map.setCenter(placemark.geometry.getCoordinates(), 17, {duration: 300}));     
+    });
+  }
+}
+
+$(() => {
+  const maps = $('.js-map').map((index, node) => new Map(node));
+});
