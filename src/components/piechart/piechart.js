5ac284c72ce959cf922fc3a0196cf69d839fc09f
prodderman <rainbowprodder@gmail.com>
2018-06-02 02:57:36 +0700

    убрать мутации в piechart #41

--- src/components/piechart/piechart.js
+++ src/components/piechart/piechart.js
@@ -5,39 +5,43 @@ class PieChart {
   }
 
   _render() {
-    const $canvas = this.$diagram.find('canvas').get(0);
-    const ctx = $canvas.getContext('2d');
-    const r = this.$diagram.width() / 2;
-    const $pieces = this.$diagram.find('.js-piechart__data figure');
-    let totalCount = 0;
-    let prevAngle = 0;
+    const canvas = this.$diagram.find('canvas').get(0);
+    const ctx = canvas.getContext('2d');
+    const radius = this.$diagram.width() / 2;
+    const pieces = this.$diagram.find('.js-piechart__data .js-piechart__data-item').toArray();
+    const totalCount = pieces.reduce(this._calcTotalCount, 0);
+    canvas.width = this.$diagram.width() * 1.5;
+    canvas.height = this.$diagram.width() * 1.5;
+    const center = canvas.width / 2;
 
-    $canvas.width = this.$diagram.width() * 1.5;
-    $canvas.height = this.$diagram.width() * 1.5;
-    const c = $canvas.width / 2;
-
-    $pieces.map((index, node) => {
-      totalCount += $(node).data('count');
-    });
-
-    $pieces.map((index, node) => {
-      const angle = prevAngle + $(node).data('count') * 2 * Math.PI / totalCount;
-      ctx.beginPath();
-      ctx.arc(c, c, r - 7.5, -Math.PI/2 + prevAngle, -Math.PI/2 + angle, false);
-      ctx.fillStyle = 'transparent';
-      ctx.fill();
-      ctx.lineWidth = 15;
-      ctx.strokeStyle = $(node).data('color');
-      ctx.stroke();
-      prevAngle = angle;
-    }, this);
+    pieces.reduce((prevAngle, currentPiece) => {
+      const angle = prevAngle + currentPiece.dataset.count * 2 * Math.PI / totalCount;
+      this._renderPieceWithAngle(ctx, center, radius, prevAngle, angle, currentPiece.dataset.color);
+      return angle;
+    }, 0);
   }
+
+  _renderPieceWithAngle(context, center, radius, startAngle, endAngle, color) {
+    context.beginPath();
+    context.arc(center, center, radius - 7.5, -Math.PI/2 + startAngle, -Math.PI/2 + endAngle, false);
+    context.fillStyle = 'transparent';
+    context.fill();
+    context.lineWidth = 15;
+    context.strokeStyle = color;
+    context.stroke();
+  }
+
+  _calcTotalCount(totalCount, currentPiece) {
+    return totalCount + parseInt(currentPiece.dataset.count);
+  }
+ 
 }
 
-function render(isElementOnPage = false) {
-  $(() => {
-    $(isElementOnPage ? '.js-layout__pjax-container .js-piechart' : '.js-piechart').map((index, node) => new PieChart(node));
-  });
+function render(isComponentOnPjaxContainer = false) {
+  const $components = $(isComponentOnPjaxContainer ? '.js-layout__pjax-container .js-piechart' : '.js-piechart');
+  if ($components.length > 0) {
+    $components.map((index, node) => new PieChart(node));
+  }
 }
 
 export default render;
